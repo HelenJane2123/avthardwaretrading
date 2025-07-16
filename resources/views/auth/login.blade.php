28f4c04753433333da9de599324f6e81aab7eeb0
unknown <HelenJane2123>
2025-07-16 17:38:23 +0800

    update design on login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,71 +1,66 @@
 @extends('layouts.master')
 
 @section('content')
-
-
-<section class="material-half-bg">
-    <div class="cover"></div>
-</section>
-<section class="login-content">
-    <div class="logo">
-        <h2>Sales and Invoice System</h2>
-    </div>
-    <div class="login-box">
-        <form class="login-form" method="POST" action="{{ route('login') }}">
-            @csrf
-            <h3 class="login-head"><i class="fa fa-lg fa-fw fa-user"></i>LOG IN</h3>
-            <div class="form-group">
-                <label class="control-label">Email</label>
-                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-                @error('email')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-                @enderror
+<section class="login-wrapper d-flex">
+    <!-- Left Side: Login -->
+    <div class="login-left d-flex flex-column justify-content-center align-items-center">
+       <div class="logo text-left w-100 px-4 d-flex align-items-center mb-4">
+            <img src="{{ asset('images/samplelogo.png') }}" alt="Logo" style="height: 130px; margin-right: 15px;">
+            <div>
+                <h1 class="mb-0">AVT Hardware Trading</h1>
+                <h5 class="mb-0">Sales and Inventory System</h5>
             </div>
-            <div class="form-group">
-                <label class="control-label">Password</label>
-                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-                 @error('email')
-                 <span class="invalid-feedback" role="alert">
-                     <strong>{{ $message }}</strong>
-                 </span>
-                 @enderror
-            </div>
-            <div class="form-group">
-                <div class="utility">
-                    <div class="animated-checkbox">
-                        <label>
-                            <input type="checkbox"><span class="label-text">Stay Signed in</span>
-                        </label>
-                    </div>
-                    <!-- <p class="semibold-text mb-2"><a href="#" data-toggle="flip">Forgot Password ?</a></p> -->
+        </div>
+        <div class="login-box px-4 w-100">
+            <form class="login-form" method="POST" action="{{ route('login') }}">
+                @csrf
+                <h5 class="login-head mb-4"><i class="fa fa-lg fa-fw fa-user"></i>LOG IN</h5>
+                <div class="form-group">
+                    <label class="control-label">Email</label>
+                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                        name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
                 </div>
-            </div>
-            <div class="form-group btn-container">
-                <button class="btn btn-primary btn-block" type="submit"><i class="fa fa-sign-in fa-lg fa-fw"></i>LOG IN</button>
-            </div>
-        </form>
-        <form class="forget-form" action="index.html">
-            <h3 class="login-head"><i class="fa fa-lg fa-fw fa-lock"></i>Forgot Password ?</h3>
-            <div class="form-group">
-                <label class="control-label">EMAIL</label>
-                <input class="form-control" type="text" placeholder="Email">
-            </div>
-            <div class="form-group btn-container">
-                <button class="btn btn-primary btn-block"><i class="fa fa-unlock fa-lg fa-fw"></i>RESET</button>
-            </div>
-            <div class="form-group mt-3">
-                <p class="semibold-text mb-0"><a href="#" data-toggle="flip"><i class="fa fa-angle-left fa-fw"></i> Back to Login</a></p>
-            </div>
-        </form>
+                <div class="form-group">
+                    <label class="control-label">Password</label>
+                    <input id="password" type="password"
+                        class="form-control @error('password') is-invalid @enderror" name="password" required
+                        autocomplete="current-password">
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <div class="utility">
+                        <div class="animated-checkbox">
+                            <label>
+                                <input type="checkbox"><span class="label-text">Stay Signed in</span>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group btn-container">
+                    <button class="btn btn-primary btn-block" type="submit">
+                        <i class="fa fa-sign-in fa-lg fa-fw"></i>LOG IN
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <!-- Right Side: Analytics or Image -->
+    <div class="login-right d-flex align-items-center justify-content-center">
+        <div class="analytics-text text-white text-center">
+            <h2>Track your Sales</h2>
+            <p>Real-time Inventory Monitoring & Analytics</p>
+            <img src="{{ asset('images/data-analysis.png') }}" alt="Sales Analytics" class="img-fluid mt-4">
+        </div>
     </div>
 </section>
-
-{{--                                @if (Route::has('password.request'))--}}
-{{--                                    <a class="btn btn-link" href="{{ route('password.request') }}">--}}
-{{--                                        {{ __('Forgot Your Password?') }}--}}
-{{--                                    </a>--}}
-{{--                                @endif--}}
-
 @endsection
